5eb74d3448cbb84a24a1200254bd7e1e41335438
Milton Chang <mchangpi@gmail.com>
2023-10-21 10:46:54 +0800

    L86 Sorting Items

--- work/80-travel-list/src/App.jsx
+++ work/80-travel-list/src/App.jsx
@@ -89,10 +89,26 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems = items;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  } else if (sortBy === "description") {
+    sortedItems = items.slice(0, items.length).sort((a, b) => {
+      return a.description.localeCompare(b.description);
+    });
+  } else if (sortBy === "packed") {
+    sortedItems = items.slice(0, items.length).sort((a, b) => {
+      return Number(a.packed) - Number(b.packed);
+    });
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -101,6 +117,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
