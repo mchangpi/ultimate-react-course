70ad125a1c927e4e2197a02ef14d1e54c6cb5dab
Milton Chang <mchangpi@gmail.com>
2023-11-03 22:54:52 +0800

    L187 Yet Another Hook: useReducer

---
+++ work/187-react-quiz/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import DateCounter from "./DateCounter";
+
+export default function App() {
+  return (
+    <div>
+      <DateCounter></DateCounter>
+    </div>
+  );
+}
