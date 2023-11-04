415ef790f7c840b0308c22dbca0910cab24fd14c
Milton Chang <mchangpi@gmail.com>
2023-11-04 23:41:34 +0800

    L191 Loading Questions from a Fake API

--- work/187-react-quiz/src/App.jsx
+++ work/187-react-quiz/src/App.jsx
@@ -1,9 +1,44 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
 // import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./components/Main";
 
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log(state, action);
+  switch (action.type) {
+    case "received":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "failed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action unkown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "received", payload: data });
+      } catch (err) {
+        // console.error("Error", err);
+        dispatch({ type: "error" });
+      }
+    }
+    fetchQuestions();
+  }, []);
+
   return (
     <div className="app">
       {/* <DateCounter></DateCounter> */}
