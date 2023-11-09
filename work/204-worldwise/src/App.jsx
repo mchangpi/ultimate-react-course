5c73c883bef82109d438974772db51fe772429be
Milton Chang <mchangpi@gmail.com>
2023-11-09 11:42:45 +0800

    L206 Implementing Main Pages and Routes

---
+++ work/204-worldwise/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h3>Hello React Router</h3>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="product" element={<Product />}></Route>
+          <Route path="pricing" element={<Pricing />}></Route>
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
