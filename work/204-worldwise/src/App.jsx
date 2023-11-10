e997cb4668ef0b1a9d2df22d9fb63f6d7ecccf42
Milton Chang <mchangpi@gmail.com>
2023-11-10 13:13:36 +0800

    L213 Implementing the Cities List

--- work/204-worldwise/src/App.jsx
+++ work/204-worldwise/src/App.jsx
@@ -19,7 +19,12 @@ function App() {
           <Route path="product" element={<Product />}></Route>
           <Route path="pricing" element={<Pricing />}></Route>
           <Route path="login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}></Route>
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<p>Index Page</p>}></Route>
+            <Route path="cities" element={<p>List of cities</p>}></Route>
+            <Route path="countries" element={<p>Countries</p>}></Route>
+            <Route path="form" element={<p>Form</p>}></Route>
+          </Route>
           <Route path="*" element={<PageNotFound />}></Route>
         </Routes>
       </BrowserRouter>
--- work/204-worldwise/src/components/Sidebar.jsx
+++ work/204-worldwise/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 
@@ -8,7 +9,10 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+
+      <Outlet />
+      {/* Oultet is similar to children, but is for nested Route */}
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by Milton Inc.
