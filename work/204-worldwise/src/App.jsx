c7a7c353d56e14958f652571a5049dea7bb0a1f0
Milton Chang <mchangpi@gmail.com>
2023-11-13 10:44:32 +0800

    L227 Back to WorldWise: Creating a CitiesContext

--- work/204-worldwise/src/App.jsx
+++ work/204-worldwise/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -11,58 +10,30 @@ import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 
-const BASE_URL = "http://localhost:8000";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
 function App() {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function () {
-    const fetchCities = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch (error) {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchCities();
-  }, []);
-
   return (
     <>
       <h3>Hello React Router</h3>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="product" element={<Product />}></Route>
-          <Route path="pricing" element={<Pricing />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            {<Route index element={<Navigate replace to="cities" />}></Route>}
-
-            <Route
-              path="cities"
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            ></Route>
-
-            {/* name the param as id*/}
-            <Route path="cities/:id" element={<City />}></Route>
-
-            <Route
-              path="countries"
-              element={<CountryList cities={cities} isLoading={isLoading} />}
-            ></Route>
-
-            <Route path="form" element={<Form />}></Route>
-          </Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="product" element={<Product />}></Route>
+            <Route path="pricing" element={<Pricing />}></Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="app" element={<AppLayout />}>
+              {<Route index element={<Navigate replace to="cities" />}></Route>}
+              <Route path="cities" element={<CityList />}></Route>
+              <Route path="cities/:id" element={<City />}></Route>
+              <Route path="countries" element={<CountryList />}></Route>
+              <Route path="form" element={<Form />}></Route>
+            </Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
     </>
   );
 }
