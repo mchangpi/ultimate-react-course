597cb65f5a58080c959fa86ca726d32bb1b1bf26
Milton Chang <mchangpi@gmail.com>
2023-11-14 10:00:58 +0800

    L233 Setting Map Position With Geolocation

--- work/204-worldwise/src/components/Map.jsx
+++ work/204-worldwise/src/components/Map.jsx
@@ -1,42 +1,61 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup,
-  useMap,
-  useMapEvents,
-} from "react-leaflet";
+import { useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { useCities } from "../contexts/CitiesContext";
 import ChangeCenter from "./ChangeCenter";
 import DetectClick from "./DetectClick";
 
+import Button from "./Button";
+
 import styles from "./Map.module.css";
+import { useGeolocation } from "../hooks/useGeolocation";
 
 function Map() {
-  const [mapPosition, setMapPosition] = useState([40, 0]);
-
   const { cities } = useCities();
 
+  /* LOCAL state: SYNC LOCAL state with GLOBAL/REMOTE state */
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geoLocation,
+    getPosition,
+  } = useGeolocation();
+
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
   useEffect(
-    // Update LOCAL mapPosition based on GLOBAL searchParams
+    // SYNC LOCAL mapPosition with GLOBAL searchParams
     function () {
-      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+      if (mapLat && mapLng) {
+        setMapPosition([mapLat, mapLng]);
+      }
     },
     [mapLat, mapLng]
   );
-  // console.log(mapLat, mapLng);
+
+  useEffect(
+    // SYNC LOCAL mapPosition with REMOTE geoLocation
+    function () {
+      if (geoLocation) {
+        setMapPosition([geoLocation.lat, geoLocation.lng]);
+      }
+    },
+    [geoLocation]
+  );
 
   /* https://react-leaflet.js.org/ */
 
   return (
     /* navigate: programmatic navigation */
     <div className={styles.mapContainer}>
+      {!geoLocation && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         center={mapPosition}
         zoom={6}
