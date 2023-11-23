b7df5a90167e7d32777d2cd43d94723c2b7b8aba
Milton Chang <mchangpi@gmail.com>
2023-11-23 23:26:51 +0800

    L297 Styling Text

--- work/282-fast-pizza/src/features/cart/CartOverview.jsx
+++ work/282-fast-pizza/src/features/cart/CartOverview.jsx
@@ -1,7 +1,7 @@
 function CartOverview() {
   return (
-    <div className="bg-stone-800 text-stone-200">
-      <p className="text-stone-300">
+    <div className="bg-stone-800 uppercase text-stone-200">
+      <p className="font-bold text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
---
+++ work/282-fast-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text font-semibold">Milton</div>;
+}
+
+export default Username;
