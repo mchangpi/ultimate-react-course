87e249dabd82f43f549007c55493f71ad8a7c5a2
Milton Chang <mchangpi@gmail.com>
2023-11-24 23:13:28 +0800

    L300 Using Flexbox

--- work/282-fast-pizza/src/features/user/Username.jsx
+++ work/282-fast-pizza/src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Milton</div>;
+  return <div className="hidden text-sm font-semibold md:block">Milton</div>;
 }
 
 export default Username;
