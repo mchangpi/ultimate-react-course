926c68de2c28220de3127f1ca8a030dfe1ea756f
Milton Chang <mchangpi@gmail.com>
2023-11-26 23:25:01 +0800

    L307 Configuring Tailwind: Custom Font Famlily

--- work/282-fast-pizza/tailwind.config.js
+++ work/282-fast-pizza/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+/* https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/config.full.js */
+// eslint-disable-next-line
 export default {
-  content: ["./**/*.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
+  content: ['./**/*.html', './src/**/*.{js,ts,jsx,tsx,css}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      /* override default fonts */
+      sans: 'Roboto Mono, monospace',
+    },
+    extend: {
+      // colors: { pizza: '#123456' },
+      fontSize: {
+        huge: ['10rem', { lineHeight: '1' }],
+        height: {
+          screen: '100dvh',
+        },
+      },
+    },
   },
   plugins: [],
 };
