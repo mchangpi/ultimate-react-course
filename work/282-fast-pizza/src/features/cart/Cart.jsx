35515f4884341c96519659f5c0cc64b7b54b8246
Milton Chang <mchangpi@gmail.com>
2023-11-28 14:31:05 +0800

    L309 Styling the Cart

--- work/282-fast-pizza/src/features/cart/Cart.jsx
+++ work/282-fast-pizza/src/features/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import Button from '../../ui/Button';
 import LinkButton from '../../ui/LinkButton';
+import CartItem from './CartItem';
 
 const fakeCart = [
   {
@@ -30,17 +31,23 @@ function Cart() {
   const cart = fakeCart;
 
   return (
-    <div>
+    <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
-      <div>
+      <ul className="divede-stone-200 mt-3 divide-y-2 border-b-2">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key}></CartItem>
+        ))}
+      </ul>
+
+      <div className="mt-6 space-x-2">
         <Button to="/order/new" type="primary">
           Order pizzas
         </Button>
         {/* <Link to="/order/new">Order pizzas</Link> */}
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
